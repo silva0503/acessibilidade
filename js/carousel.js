feb3ce0b8b4331a0e74e8011d8d56fce79981802
silva0503 <santos.jhenifer19@escola.pr.gov.br>
2025-11-07 08:39:43 -0300

    carousel

---
+++ js/carousel.js
@@ -0,0 +1,13 @@
+var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
+
+//Percorrer todos os botoes controladores
+btns.forEach(fonction(bnt) {
+    btn.addEventListener('click', fonction()) {
+
+
+        //Remover class 'ativo' dos outros botoes
+        btns.forEach(function(btnRemoveclass) {
+            btnRemoveclass.ClassList.remove('listaDeArtigos-slider-item-ativo')
+        }
+    }
+}
